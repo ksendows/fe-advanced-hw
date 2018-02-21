1b58ffa3928ba7cc72cff371350275f672ebc37c
Ksenia Gromakovska <ksendows@gmail.com>
2018-02-21 20:11:59 +0200

    timer visualization added

--- module14/index.js
+++ module14/index.js
@@ -4,21 +4,57 @@
 // При нажатии на кнопку stop, функция сохраняет значение текущего момента времени в stopTime и записывает разницу между startTime и stopTime в interval.
 // При нажатии на stop, значение interval выводится в консоль.
 
+const timer = {
+  currentTime: 0,
+  id: ""
+};
+
+const timerOutput = document.querySelector("#timer");
+const startBtn = document.querySelector('#start-button');
+const stopBtn = document.querySelector('#stop-button');
+
 const Timer = function () {
 }
 
 Timer.prototype.start = function () {
-  this.startTime = new Date;  
+  this.startTime = new Date; 
+
+  if (!timer.id) {
+    timer.id = setInterval(() => {
+      timer.currentTime += 4;
+      timerOutput.textContent = getFormattedTime(timer.currentTime);
+    }, 4);
+  }
 }
 
 Timer.prototype.stop = function () {
   this.stopTime = new Date;
   this.interval = this.stopTime - this.startTime;
-  if (this.interval) {console.log(`Прошло ${Math.round(this.interval/10)/100} секунд с последнего нажатия клавиши Start`)};
+
+  if (this.interval && timer.id) {console.log(`Прошло ${Math.round(this.interval/100)/10} секунд с нажатия клавиши Start`)};
+
+  clearInterval(timer.id);
+  timer.id = '';
+  timer.currentTime = 0;
 }
 
-const startBtn = document.querySelector('#start-button');
-const stopBtn = document.querySelector('#stop-button');
+
+function getFormattedTime(time) {
+  const date = new Date(time);
+  const mt =
+    date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+  const sc =
+    date.getSeconds() > 9 ? date.getSeconds() : "0" + date.getSeconds();
+  const ms =
+    date.getMilliseconds() < 10
+      ? "00" + date.getMilliseconds()
+      : date.getMilliseconds() < 100
+        ? "0" + date.getMilliseconds()
+        : date.getMilliseconds();
+
+  return `${mt}:${sc}:${ms}`;
+}
+
 
 const a = new Timer();
 startBtn.addEventListener('click', () => {a.start()});
